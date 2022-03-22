c2f45f91070b538e16e8cf286b764c97c36b2719
Tim Kaerts <tkaerts@avans.nl>
2022-03-22 12:36:24 +0100

    Initial Commit

---
+++ MBDI_WS2_CookieClicker/MBDI_WS2_CookieClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBDI_WS2_CookieClickerApp.swift
+//  MBDI_WS2_CookieClicker
+//
+//  Created by Tim Kaerts on 22/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MBDI_WS2_CookieClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
